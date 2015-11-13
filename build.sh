cb2e5b8f47c0bc5d4afbecf0464e4c7347761648
Jose Rivera <jose.rivera@imperial.ac.uk>
2015-11-13 00:19:13 +0000

    Subsituted build script for Makefile. GNU make allows more flexibility

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pdflatex -synctex=1 -interaction=nonstopmode jmrrThesis.tex
-pdflatex -synctex=1 -interaction=nonstopmode jmrrThesis.tex
-bibtex -synctex=1 -interaction=nonstopmode jmrrThesis.tex
-pdflatex -synctex=1 -interaction=nonstopmode jmrrThesis.tex
-
-
